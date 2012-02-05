abd4ff831b0a0bf64c5b9cefd3c8551ab548b080
Greg Akins <angrygreg@gmaail.com>
2012-02-05 10:13:24 -0500

    Multiple ThingSpeak widgets laid out in a constraining div

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//=require jquery-ui-1.8.17.custom.min.js
+//= require jquery-ui-1.8.17.custom.min.js
+//= require jquery.window.js
 //= require_tree .
 
